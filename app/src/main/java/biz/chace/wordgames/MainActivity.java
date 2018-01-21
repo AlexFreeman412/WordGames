351bb62c0cbb4c63591707ca71455e165840a4d8
AlexFreeman412 <freeman_412@hotmail.com>
2018-01-21 11:43:15 +0000

    Added finish screen and percentage score calc

--- app/src/main/java/biz/chace/wordgames/MainActivity.java
+++ app/src/main/java/biz/chace/wordgames/MainActivity.java
@@ -1,39 +1,58 @@
 package biz.chace.wordgames;
 
-import android.support.v4.content.res.TypedArrayUtils;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String wordList[] = {"aah","aha","ate","awe","aye","eat","eta","ewe","eye","hat","haw","hay","het","hew","hey","taw","tea","tee","tet","the","thy","waw","way","wee","wet","why","wye","yah","yaw","yay","yea","yeh","yet","yew"};
+    //private String wordList[] = {"aah","aha","ate","awe","aye","eat","eta","ewe","eye","hat","haw","hay","het","hew","hey","taw","tea","tee","tet","the","thy","waw","way","wee","wet","why","wye","yah","yaw","yay","yea","yeh","yet","yew"};
     private TextView CountTextView;
     private TextView LetterDisplay;
 
     private char[] lettersGenerated;
     private char[] lettersSubmitted;
 
+    private ArrayList<String> wordList;
+    private ArrayList<String> correctGuesses;
+
+    final Context context = this;
+
+    private int numCorrect = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CountTextView = (TextView)findViewById(R.id.CountTextView);
-        LetterDisplay = (TextView)findViewById(R.id.LetterDisplay);
+        CountTextView = findViewById(R.id.CountTextView);
+        LetterDisplay = findViewById(R.id.LetterDisplay);
+
+        StartGame();
+    }
+
+    protected void StartGame(){
+        numCorrect = 0;
+        CountTextView.setText(Integer.toString(numCorrect));
 
         lettersGenerated = GenerateRandomLetters();
+        wordList = GetWords(lettersGenerated);
+
         shuffleArray(lettersGenerated);
         String letters = new String(lettersGenerated);
         LetterDisplay.setText(letters);
 
+        TestGame();
+    }
+
+    protected void TestGame(){
         Random rnd = new Random();
         char[] toSubmit = new char[3];
         do{
@@ -44,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
 
         //SubmitCharacters('e', 'y', 'e');
         SubmitCharacters(toSubmit[0],toSubmit[1],toSubmit[2]);
+
+        ShowFinishScreen();
     }
 
     protected char[] GenerateRandomLetters(){
@@ -58,6 +79,21 @@ public class MainActivity extends AppCompatActivity {
         char[] allLetters = sb.toString().toCharArray();
 
         return allLetters;
+    }
+
+    protected ArrayList<String> GetWords(char[] letters){
+        ArrayList<String> validWords = new ArrayList<String>();
+        for(String word: Words.threeLetterWords){
+            boolean isValidWord = true;
+            for(char character: word.toCharArray()){
+                if(!contains(letters,character)){
+                    isValidWord = false;
+                    break;
+                }
+            }
+            if(isValidWord) validWords.add(word);
+        }
+        return validWords;
     }
 
     protected char[] GetVowels(){
@@ -125,10 +161,8 @@ public class MainActivity extends AppCompatActivity {
 
         String word = CombineCharacters(one, two, three);
 
-        if(CheckWord(word)){
-            int currentCount = Integer.parseInt(CountTextView.getText().toString());
-            CountTextView.setText(Integer.toString(currentCount+1));
-        }
+        if(CheckWord(word))
+            CountTextView.setText(Integer.toString(numCorrect++));
     }
 
     protected String CombineCharacters(char one, char two, char three){
@@ -152,10 +186,43 @@ public class MainActivity extends AppCompatActivity {
                 usesRightLetters = false;
         }
 
-        boolean isRealWord = Words.IsThreeLetterWord(word);
+        //boolean isRealWord = Words.IsThreeLetterWord(word);
+        boolean isRealWord = contains(wordList.toArray(),word);
 
         return usesRightLetters & isRealWord;
 
+    }
+
+    protected void ShowFinishScreen(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set title
+        alertDialogBuilder.setTitle("Finished");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("Congratulations you correctly guessed " + numCorrect + " out of the total "
+                + wordList.toArray().length + " words! You got a score of " + CalcScoreAsPercentage() + "%!")
+                .setCancelable(false)
+                .setNeutralButton("Restart",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        StartGame();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    protected float CalcScoreAsPercentage(){
+        float score = (( (float)numCorrect / (float)wordList.toArray().length) * 100);
+        return round(score, 2);
     }
 
     public static <T> boolean contains(final T[] array, final T v) {
@@ -187,5 +254,13 @@ public class MainActivity extends AppCompatActivity {
             ar[index] = ar[i];
             ar[i] = a;
         }
+    }
+
+    public static float round(float number, int scale) {
+        int pow = 10;
+        for (int i = 1; i < scale; i++)
+            pow *= 10;
+        float tmp = number * pow;
+        return ( (float) ( (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) ) ) / pow;
     }
 }
